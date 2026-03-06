a544cf3d1627345c30d9a5a18565220d0fa70237
bijafarm <bijaagrofarm@gmail.com>
2026-03-06 10:28:03 +0530

    EmployeeModel class created

---
+++ src/Model/Employee.ts
@@ -0,0 +1,19 @@
+export class EmployeeModel {
+    empId: string;
+    name: string;
+    city: string;
+    state: string;
+    emailId: string;
+    phoneNumber: string;
+    address: string;
+}
+
+constructor(){
+    this.empId=0;
+    this.name="";
+    this.city="";
+    this.state="";
+    this.emailId="";
+    this.phoneNumber="";
+    this.address="";
+}
